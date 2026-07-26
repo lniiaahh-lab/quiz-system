efb4f287b2091446d6b3d7c00ebc174cedee0ebe
lniiaahh-lab <Iniiaahh@gmail.com>
2026-07-26 14:26:10 +0300

    feat: add Question interface

---
+++ dist/models/Question.d.ts
@@ -0,0 +1,9 @@
+export interface Question {
+    id: number;
+    text: string;
+    options: string[];
+    correctAnswer: number;
+    difficulty: 'easy' | 'medium' | 'hard';
+    points: number;
+}
+//# sourceMappingURL=Question.d.ts.map
